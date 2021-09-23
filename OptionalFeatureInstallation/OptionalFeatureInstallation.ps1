f279b5f13ecb6d9c6a6b0b0118be370281833178
blurrybronson <40902983+blurrybronson@users.noreply.github.com>
2021-09-22 21:01:53 -0500

    Add files via upload
    Initial upload of the OptionalFeatureInstallation project

---
+++ OptionalFeatureInstallation/OptionalFeatureInstallation.ps1
@@ -0,0 +1,8 @@
+﻿# looping through text file containing feature names to enable optional features
+# adding no restart flag to avoid being prompted to reboot after each feature is enabled 
+foreach($feature in Get-Content -Path "OptionalFeatureNames.txt"){
+    Enable-WindowsOptionalFeature -FeatureName $feature -Online -All -NoRestart
+}
+
+# restarting the machine 
+Restart-Computer -Force
