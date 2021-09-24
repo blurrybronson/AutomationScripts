c8cff4fb8b6d93f82ca0282f50111d2fb3d7689c
blurrybronson <40902983+blurrybronson@users.noreply.github.com>
2021-09-23 21:09:00 -0500

    Add files via upload
    Added code to get ssh packages, install them, and enable the sshd service.

--- OptionalFeatureInstallation/OptionalFeatureInstallation.ps1
+++ OptionalFeatureInstallation/OptionalFeatureInstallation.ps1
@@ -4,5 +4,14 @@ foreach($feature in Get-Content -Path "OptionalFeatureNames.txt"){
     Enable-WindowsOptionalFeature -FeatureName $feature -Online -All -NoRestart
 }
 
+# installing openssh
+$capability = Get-WindowsCapability -Online | Where-Object Name -Like "OpenSSH*"
+foreach($name in $capability.Name){
+    Add-WindowsCapability -Online -Name $name
+}
+
+# setting ssh to run at startup
+Set-Service sshd -StartupType Automatic
+
 # restarting the machine 
 Restart-Computer -Force
